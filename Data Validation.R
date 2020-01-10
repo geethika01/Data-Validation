ea79212ce082dc2d1201ac9d06c86101224759de
Geethika Wijewardena <Geethika.Wijewardena@racgp.org.au>
2020-01-10 22:16:28 +1100

    Major changes to the code. 1. Added functions to validate email, DOB and over18 at individual scale and then across a list 2. Revised rules using the new functions

--- Data Validation.R
+++ Data Validation.R
@@ -1,14 +1,40 @@
+#
+# Title: Validation of tabular dataset using the Validate package
+# Description: This script generates a POI dataset and infuse teh data with some
+#             erroneous data vaules. Then it validate the data types, formats and
+#             business rules accordeing to a predefined rules set.
+# Outputs: (a) Summary of issue counts 
+#          (b) Individual data issues
+# Written by: Geethika Wijewardena
+# Date: 20 Jan 2020
+#-------------------------------------------------------------------------------
+#---------------------------------------------------
+# Setup
+#---------------------------------------------------
 rm(list = ls())
 library(dplyr)
 library(stringr)
 library(validate)
 library(lubridate)
 library(generator)
-library(skimr)
 
 #---------------------------------------------------
 # Functions
 #---------------------------------------------------
+isDuplicated<- function(str_lst){
+  # Function to check if each value in a list contain a duplicate value
+  # Args : list of values
+  # Returns : list of TRUE/FALSE/NA corresponding to each vaue on if it contains
+  #   duplicates or not
+  str_dup <- str_lst[duplicated(str_lst)]
+  result_lst<- c()
+  for(i in 1:length(str_lst)){
+    result_lst[i]<- case_when(is.na(str_lst[i]) ~ NA,
+                              (str_lst[i] %in% str_dup) ~F,
+                              TRUE ~ T)
+  }
+  return(result_lst)
+}
 
 isUpperCase <- function(names_lst){
   # Function to evaluate if a given list of values are uppercase
@@ -25,63 +51,103 @@ isUpperCase <- function(names_lst){
   return(result_lst)
 }
 
-isValidEmail <- function(email_lst) {
+isValidEmail <- function(email) {
   # Function to check if email addresses in a given list of emails has the 
   #   correct format
   # Args : list of email addresses
   # returns : list of TRUE/FALSE/NA corresponding to each email 
+
+  result <- ifelse(is.na(email), NA, 
+                  grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>"
+                      , as.character(email), ignore.case=TRUE))
+  
+  return(result)
+}
+
+isValidEmailList <- function(emaillist){
+  # Function that evaluates each email in a given list is valid using the 
+  # isValidEmail()
+  # Args: list of emails
+  # Returns: logical vector 
   result_lst <- c()
-  for (i in 1:length(email_lst)){
-    result_lst[i] <- ifelse(is.na(email_lst[i]), NA, 
-                            grepl("\\<[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\>"
-                                  , as.character(email_lst[i]), ignore.case=TRUE
-                                  ))
+  for(i in 1: length(emaillist)){
+    result_lst[i] <- isValidEmail(emaillist[i])
   }
   return(result_lst)
 }
 
 
-isDuplicated<- function(str_lst){
-  # Function to check if each value in a list contain a duplicate value
-  # Args : list of values
-  # Returns : list of TRUE/FALSE/NA corresponding to each vaue on if it contains
-  #   duplicates or not
-  str_dup <- str_lst[duplicated(str_lst)]
-  result_lst<- c()
-  for(i in 1:length(str_lst)){
-    result_lst[i]<- case_when(is.na(str_lst[i]) ~ NA,
-                              (str_lst[i] %in% str_dup) ~F,
-                              TRUE ~ T)
+isValidDOB <- function(dob){
+  # Assess if the given date of birth is valid, i.e. if its a valid date in yyyy-mm-dd
+  # format and less than the current date
+  # Args: character or null value
+  # returns: TRUE of FALSE
+  
+  dob_date <- ymd(dob)
+  dob_valid <- case_when(is.na(dob_date)== T ~ F,
+                         dob_date > Sys.Date() ~ F,
+                         (nchar(dob)==10 & grepl('[0-9]{4}[-][0-9]{2}[-][0-9]{2}',dob))== F ~ F,
+                         TRUE ~ T)
+  
+  return(dob_valid)
+}
+
+isValidDOBList <- function(doblist){
+  # Function that evaluates each dateofbirth in a given list is valid using the 
+  # isValidDOB()
+  # Args: list of dates
+  # Returns: logical vector
+  result_lst <- c()
+  for(i in 1: length(doblist)){
+    result_lst[i] <- ifelse(is.na(doblist[i]), NA,isValidDOB(doblist[i]))
   }
   return(result_lst)
 }
 
+isValidOver18 <- function(dob, over18){
+  # Function to calculate if a person is over 18 years, given the DOB and compare 
+  # it with the given value
+  # Args: DOB, vaule for over18 field
+  # Returns: if calculated value and given value are the same => T else F
 
-isValidOver18 <- function(dateofbirth.lst, over18.lst){
+  # Check if the DOB is valid  
+  dob_valid <- isValidDOB(dob)
+  #dob_valid
+  # Check if the calculation is correct
+  num_years <- NA
+  if (unlist(dob_valid) == T) {
+    num_years <- trunc(time_length(interval(ymd(dob),Sys.Date()), "year"))
+  } 
+  
+  # For over18 to be valid, 
+  # a) dob should be valid
+  # b) if dob is valid, then check for number of years
+  # c) now check for over18
+  #    if DOB is invalid, over18 = NA
+  #    if dob is valid, 
+  #    if number of years > 18, over18 = 1 
+  #    if number of years < 18 over18 = 0
+  
+  over18_calc <- case_when(dob_valid == F ~ NA_character_,
+                           dob_valid == T & num_years > 18 ~ "1",
+                           dob_valid == T & num_years < 18 ~ "0")
+  valid_over18 <- case_when(is.na(over18_calc) & is.na(over18) ~ T,
+                            over18_calc == over18 ~ T,
+                            TRUE ~ F)
+  
+  return(valid_over18)
+}
+
+isValidover18List <- function(doblst,over18lst){
   # Function to assess data in a list of over18 is correct given the 
   #   corresponding date of birth
   # Args : lists of date of birth and corresponding over18 
   # returns : list of TRUE/FALSE/NA corresponding to each vaue of the over18 
   result_lst <- c()
-  for (i in 1:length(over18_lst)){
-    # Check if the cauculation is correct
-    result_1 <- case_when(is.na(trunc(time_length(
-                            interval(ymd(dateofbirth_lst[i]),Sys.Date()), 
-                                                  "year")))== T ~ NA_character_,
-                          trunc(time_length(
-                            interval(ymd(dateofbirth_lst[i]),Sys.Date()),
-                                                  "year")) >= 18 ~ "1",
-                          TRUE ~ "0" ) 
-    result_lst[i] <- case_when(is.na(result_1) & is.na(over18_lst[i]) ~ NA,
-                               is.na(result.1) & !is.na(over18_lst[i]) ~ F,
-                               !is.na(result.1) & is.na(over18_lst[i]) ~ F,
-                               result_1 == over18.lst[i] ~ T,
-                               TRUE ~ F )
-    
+  for(i in 1: length(over18lst)){
+    result_lst[i] <- isValidOver18(doblst[i],over18lst[i])
   }
-  
-  
-  return(result.lst)
+  return(result_lst)
 }
 
 #----------------------------------------------------
@@ -106,7 +172,7 @@ gender <- c(rep("M", 750), rep("F", 250))
 dat <- cbind.data.frame(id, firstname, lastname, dateofbirth, email, phone, 
                         gender, stringsAsFactors = F) 
 # ---------------------------------------------
-# Data Alteration for testing
+# Infusion of data with issues
 #-----------------------------------------------
 # id
 #---------------------
@@ -121,15 +187,17 @@ dat$id[21:30] <-sub("[[:digit:]]", "", dat$id[21:30])
 dat$id[31:40] <-sub("[[:digit:]]", "00", dat$id[31:40])
 # Create Duplicated IDs in the next 10 rows
 dat$id[41:50] <- dat$id[51:60]
+
 #-----------------------
 # first name
 #-------------
 # Replace the 1st character of the 1st ten records with a digit
 dat$firstname[1:10] <-sub("[[:alpha:]]", "0", dat$firstname[1:10])
-# Replace the 1st character of the next ten recordswith lowercase
+
+# Firstname - lowercase
 dat <- dat %>% mutate_at(vars(firstname),funs(ifelse(between(row_number(),11,20)
                                                      , tolower(.), .)))
-# Make the first name Null for nest 10 rows
+# Make the first name Null for next 10 rows
 dat <- dat %>% mutate_at(vars(firstname),funs(ifelse(between(row_number(),21,30)
                                                      , NA, .)))
 head(dat)
@@ -152,12 +220,22 @@ dat <- dat %>% mutate_at(vars(dateofbirth),
 # Make the next 10 records of Date the Birthdate  NA
 dat <- dat %>% mutate_at(vars(dateofbirth), 
                          funs(ifelse(between(row_number(),31,40), NA, .)))
-# Make the birth year of next 10 rows = 2005 so that they are definelty below 18
+# Make the birth year of next 10 rows = 2015 so that they are definelty below 18
 dat <- dat %>% mutate_at(vars(dateofbirth), 
                          funs(ifelse(between(row_number(),41,50), 
-                                     str_replace(.,str_sub(.,1,4),"2005"), .)))
-# Create Duplicate DateofBirth in next 10 rows
-dat$dateofbirth[51:60] <- dat$dateofbirth[61:70]
+                                     str_replace(.,str_sub(.,1,4),"2015"), .)))
+# Make the year of the next 10 records > year of the current date such that the 
+# dateOfBirth is invalid
+dob_yr_change_dat <- dat$dateofbirth[51:60]
+year_current <- year(Sys.Date())
+
+for (i in 1:10){
+  alt_year <- as.numeric(year_current) + i
+  dob_yr_change_dat[i] <- paste0(as.character(alt_year),
+                                 str_sub(dob_yr_change_dat[i],5,10))
+}
+dat$dateofbirth[51:60] <- dob_yr_change_dat
+
 #-----------------
 # Email
 #-----------------
@@ -208,6 +286,7 @@ dat <- dat %>% mutate_at(vars(phone),
                            ))
 # phone should be unique. Create duplicated emails in the next 10 rows
 dat$phone[51:60] <- dat$phone[61:70]
+
 #---------------------
 # Gender
 #----------------------
@@ -218,9 +297,10 @@ dat <- dat %>% mutate_at(vars(gender),
 dat <- dat %>% mutate_at(vars(gender),
                          funs(ifelse(between(row_number(),11,20), 
                                      str_replace(.,"M", "Male"), .)))
-#------------------------------------------
-# Calculate over 18 and credit card number
-#------------------------------------------
+
+#----------------------
+# Calculate over 18 
+#----------------------
 dat <- dat %>% mutate(over18 = case_when(
   is.na(trunc(time_length(interval(ymd(dateofbirth),Sys.Date()), "year")))== T 
                                                                 ~ NA_character_,
@@ -230,80 +310,82 @@ dat <- dat %>% mutate(over18 = case_when(
 #------------------------------------------
 # Alter over 18 for testing
 #------------------------------------------
+# dateofbirth of the first 40 rows is not valid. Thus we are not going to alter 
+# the over18 field, but going to see what returns from the calculation
+# Introduce "1" to the next 10 rows. Year of these roes is 2015. 
+# Thus they should be < 18. Therefore ass data to make the calculation incorrect.
+
+dat <- dat %>% mutate_at(vars(over18), 
+                         funs(ifelse(between(row_number(),41,50), "1", .)))
+
 # Make the 1st 10 rows NA regardless the birthdate is valid or not
 dat <- dat %>% mutate_at(vars(over18), 
-                         funs(ifelse(between(row_number(),1,10), NA, .)))
+                         funs(ifelse(between(row_number(),51,60), NA, .)))
 # Over18 is a logical column where only NA, 0 or 1 are possible.
 # Introduce 2 and "A" to fail this rule
 dat <- dat %>% mutate_at(vars(over18), 
-                         funs(ifelse(between(row_number(),11,20), "2", .)))
+                         funs(ifelse(between(row_number(),61,70), "2", .)))
 dat <- dat %>% mutate_at(vars(over18),
-                         funs(ifelse(between(row_number(),21,30), "A", .)))
-# Introduce "1" to the next 10 rows regardless of the birthday to make the 
-#   calculation incorrect, the 10 rows whose birthdate is NA is skipped
-dat <- dat %>% mutate_at(vars(over18), 
-                         funs(ifelse(between(row_number(),41,50), "1", .)))
+                         funs(ifelse(between(row_number(),71,80), "A", .)))
+
 
 #----------------------------------------------------------------
 # Data Validation
 #----------------------------------------------------------------
-labels.lst <-c(#"No empty rows"
-  #, "Column names are correct"
+
+labels_lst <-c(
     "id - consists only 9 digits"
   , "id - unique"
-  # Check for columns that should contain only letters
   , "firstname - contains no digits"
-  , "lastname - contains no digits"
   , "firstname - Uppercase"
+  , "lastname - contains no digits"
   , "lastname - Uppercase"
-  , "birthdate - in YYYY-mm-dd format"
+  , "dateofbirth - ia a valid date in YYYY-mm-dd format and less than current date"
   , "email - in valid format"
   , "email - is unique"
   , "phone - in correct format (XXX XXX XXXX)"
   , "phone - is unique"
   , "gender - either M or F"
-  , "over18 - valid values 1,0, NA"
-  , "over18 - calculation correct"
-
+  , "over18 - valid values 1,0, NA and calculation is correct"
 ) 
 
-
-rules.lst <- c(
+rules_lst <- c(
   # id
   "ifelse(!is.na(dat$id),(nchar(dat$id)== 9 & grepl('[0-9]{9}', dat$id)),NA)== T"
   , "isDuplicated(dat$id)==T"
   # firstname
-  , "ifelse(!is.na(dat$firstname), grepl('[^0-9]', dat$firstname), NA) == T"
+  , "ifelse(!is.na(dat$firstname), grepl('\\\\d', dat$firstname)==F, NA) == T"
   , "isUpperCase(dat$firstname)==T"
   # lastname
-  , "ifelse(!is.na(dat$lastname), grepl('[^0-9]', dat$lastname), NA) == T"
+  , "ifelse(!is.na(dat$lastname), grepl('\\\\d', dat$lastname)==F, NA) == T"
   , "isUpperCase(dat$lastname)==T"
-  # Birthdate
-  , "ifelse(!is.na(dat$dateofbirth), 
-            nchar(dat$dateofbirth)==10 & grepl('[0-9]{4}[-][0-9]{2}[-][0-9]{2}',
-            dat$dateofbirth), NA)==T"
-  # Email
-  , "isValidEmail(dat$email)==T"
+  # dateofbirth
+  , "isValidDOBList(dat$dateofbirth)==T"
+  # email
+  , "isValidEmailList(dat$email)==T"
   , "isDuplicated(dat$email)==T"
   # Phone Number
   , "ifelse(!is.na(dat$phone), 
             (grepl('[0-9]{3}[ ][0-9]{3}[ ][0-9]{4}',dat$phone) &
                                           nchar(dat$phone) == 12), NA)==T"
   , "isDuplicated(dat$phone)==T"
-  # Gender
+  # gender
   , "ifelse(!is.na(dat$gender), dat$gender %in% c('M', 'F'), NA)==T"
-  # Over18
-  , "ifelse(!is.na(dat$over18), dat$over18 %in% c('1', '0'), NA)==T"
-  , "isValidOver18(dat$dateofbirth, dat$over18)==T"
+  # over18
+  , "isValidover18List(dat$dateofbirth,dat$over18)==T"
+  
 ) 
 
-df <- data.frame(label = labels.lst, rule = rules.lst)
+# Data validation and generate summary
+df <- data.frame(label = labels_lst, rule = rules_lst)
 v <- validator(.data = df)
 cf <- confront(dat,v)
 quality <- as.data.frame(summary(cf))
 measure <- as.data.frame(v)
 result_validation <- (merge(quality,measure)) %>% 
   select(label, items, passes, fails, nNA, error, warning)
+
+write.csv(result_validation, "Data validation summary.csv", row.names = F)
 
 # Identify errors in the rules set
 result_validation_error <- result_validation %>% filter(error == T)
@@ -314,19 +396,29 @@ if(nrow(result_validation_error) != 0) {
   print("INFO: No errors in the data validation rules")
   # Data that fails validation are pulled out only if there are no errors in the
   # data validation rules
-  fail_vals <- data.frame(values(cf)[1])
+  fail_vals <- data.frame(values(cf))
+  #names(fail_vals) <- labels_lst
   fail_vals <- as.matrix(fail_vals)
   fail_vals<- as.data.frame(which(fail_vals==0, arr.ind=TRUE))
-  fail_vals <- mutate(fail_vals, label = labels_lst[fail_vals$col +1])%>% 
+  fail_vals <- mutate(fail_vals, label = labels_lst[fail_vals$col])%>% 
     select(-col) %>%
     mutate(id = dat[fail_vals$row, 1])
   vals <- c()  
   for (i in 1:nrow(fail_vals)){
-     vals[i] <- dat[fail_vals$row[i], 
-                            grep(str_split(fail_vals$label[i], " - ")[[1]][1],
-                                 names(dat))]
+    #i = 335
+    
+     vals[i] <- dat[fail_vals$row[i],
+                            str_split(fail_vals$label[i], " - ")[[1]][1]]
+    # print(fail_vals$row[i])
+    # print(str_split(fail_vals$label[i], " - ")[[1]][1])
+                        
   }
   fail_vals <- cbind(fail_vals,vals)
+  write.csv(fail_vals, "Individual data with issues.csv", row.names = F)
 }
+
+#------------------------------------
+# END
+#------------------------------------
 
 
